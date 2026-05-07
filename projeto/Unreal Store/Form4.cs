0347f52019724844d428bd48dad384f6fb9d6885
Rodrigo castanheira <rodrigocastanheira2008@gmail.com>
2026-05-07 11:35:01 +0100

    07/05/2026

--- projeto/Unreal Store/Form4.cs
+++ projeto/Unreal Store/Form4.cs
@@ -26,5 +26,41 @@ namespace Unreal_Store
         {
             Application.Exit();
         }
+
+        // Voltar: reexibir Form3 e fechar Form4
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // If Form4 was opened modal with Form3 as owner (ShowDialog(this)),
+            // Owner will be the Form3 instance.
+            if (this.Owner is Form3 ownerF3)
+            {
+                try
+                {
+                    if (!ownerF3.Visible) ownerF3.Show();
+                    ownerF3.WindowState = FormWindowState.Normal;
+                    ownerF3.BringToFront();
+                    ownerF3.Activate();
+                }
+                catch { /* non-fatal */ }
+            }
+            else
+            {
+                // Fallback: find any open Form3 and show it
+                var openF3 = Application.OpenForms.OfType<Form3>().FirstOrDefault();
+                if (openF3 != null)
+                {
+                    try
+                    {
+                        if (!openF3.Visible) openF3.Show();
+                        openF3.WindowState = FormWindowState.Normal;
+                        openF3.BringToFront();
+                        openF3.Activate();
+                    }
+                    catch { /* non-fatal */ }
+                }
+            }
+
+            this.Close();
+        }
     }
 }
