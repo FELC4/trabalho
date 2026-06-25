3db2e8779a5da53bb64fb1a8b6fe5f2bb025a7a2
Rodrigo castanheira <rodrigocastanheira2008@gmail.com>
2026-06-25 11:39:41 +0100

    25/06/2026

--- projeto/Unreal Store/Program.cs
+++ projeto/Unreal Store/Program.cs
@@ -12,27 +12,8 @@ namespace Unreal_Store
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            // Testar conexão com erro detalhado
-            try
-            {
-                string connectionString = @"(localdb)\MSSQLLocalDB;Database=Unreal_StoreBD;Integrated Security=True;";
-                using (var conn = new SqlConnection(connectionString))
-                {
-                    conn.Open();
-                    MessageBox.Show("Conexão bem-sucedida!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(
-                    $"Erro de conexão detalhado:\n\n" +
-                    $"Mensagem: {ex.Message}\n\n" +
-                    $"Stack Trace:\n{ex.StackTrace}",
-                    "Erro de Conexão",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Error);
-                return;
-            }
+            
+            
 
             Application.Run(new Form1());
         }
